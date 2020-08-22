029a085e15158eb2efd682e071de3acd766a6507
miki623 <68592086+miki623@users.noreply.github.com>
2020-08-22 20:31:41 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,28 +3,5 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-  def new
-    @item = Item.new
-  end
-
-  def create
-    item = Item.new(item_params)
-    if item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-
-
-  private
-
-  def item_params
-    params.require(:item).permit(:name, :info, :price, :image)
-  end
 
 end
