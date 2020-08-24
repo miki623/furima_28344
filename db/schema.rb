1c2d28ee9f7cba01f065e48aa3ab3406b3f3f348
miki623 <68592086+miki623@users.noreply.github.com>
2020-08-24 23:34:18 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_064016) do
+ActiveRecord::Schema.define(version: 2020_08_17_102915) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,18 +34,19 @@ ActiveRecord::Schema.define(version: 2020_08_18_064016) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "name", null: false
     t.text "info", null: false
     t.integer "price", null: false
-    t.integer "category", null: false
-    t.integer "sales_status", null: false
-    t.integer "delivery_fee", null: false
-    t.integer "shipping_area", null: false
-    t.integer "scheduled_delivery", null: false
+    t.integer "category_id", null: false
+    t.integer "sales_status_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "shipping_area_id", null: false
+    t.integer "scheduled_delivery_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -65,7 +66,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_064016) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "items", "users"
 end
