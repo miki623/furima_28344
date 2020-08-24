b3b1e75fc7e6842731a48dfeb6384b0398c0ee1c
miki623 <68592086+miki623@users.noreply.github.com>
2020-08-24 23:35:54 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,26 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :image, :category_id, :sales_status_id, :delivery_fee_id, :shipping_area_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+  end
 
 end
