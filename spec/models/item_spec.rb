e28800f4676c6934d1274725be7aca8dd67df422
miki623 <68592086+miki623@users.noreply.github.com>
2020-08-25 11:46:37 +0900

    モデル単体テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -73,6 +73,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it "priceが¥300未満の場合出品できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
     end
 
   end
