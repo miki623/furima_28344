e45ebd4fefceed65d17dc1726d5533ca5eea7b46
miki623 <68592086+miki623@users.noreply.github.com>
2020-09-05 18:01:37 +0900

    commit

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,44 @@
+crumb :root do
+  link "Home", root_path
+end
+
+crumb :sign_up_user do
+  link "ユーザー新規登録", new_user_registration_path
+  parent :root
+end
+
+crumb :sign_in_user do
+  link "ユーザーログイン", new_user_session_path
+  parent :root
+end
+
+crumb :item_show do |item|
+  link "商品の詳細", item_path(item)
+  parent :root
+end
+
+# crumb :item_show do
+#   link "商品の詳細", item_path(params[:id])
+#   parent :root
+# end
+
+crumb :new_item do
+  link "出品商品の登録", new_item_path
+  parent :root
+end
+
+crumb :item_edit do
+  link "商品の編集", edit_item_path(params[:id])
+  parent :item_show
+end
+
+crumb :transaction do |item|
+  link "商品の購入", item_transactions_path(params[:item_id])
+  parent :item_show, item
+end
+
+# crumb :transaction do
+#   link "商品の購入", item_transactions_path(params[:item_id])
+#   parent :root
+# end
+
